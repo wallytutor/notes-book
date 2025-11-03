11cfa4ba3b2897481e3c067d9d1bb9b6ceec3517
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-11-03 15:52:33 +0100

    Adding study

---
+++ python/thermo/simulating-dsc-atg.py
@@ -0,0 +1,24 @@
+# +
+# # %pip install -e ../../../
+# -
+
+import cantera as ct
+import majordome as mj
+
+ct.add_directory(mj.common.DATA)
+
+species = ct.Species.list_from_file("materials.yaml")
+species
+
+species[0].thermo.cp(1000) / 1000, species[0].molecular_weight
+
+solids = ct.Solution("materials.yaml", "aszo")
+solids.TP = 1000, None
+
+solids.cp_mass * (species[0].molecular_weight / 1000)
+
+solids.mass_fraction_dict()
+
+solids.species_names
+
+
